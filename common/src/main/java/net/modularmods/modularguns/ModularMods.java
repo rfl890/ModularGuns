90ad6eb77579ee3e5e4e1597be8998fe854b3ea9
Protoxy <30320751+Protoxy22@users.noreply.github.com>
2022-07-14 22:24:43 +0200

    initial commit

---
+++ common/src/main/java/net/modularmods/modularguns/ModularMods.java
@@ -0,0 +1,9 @@
+package net.modularmods.modularguns;
+
+public class ModularMods {
+    public static final String MOD_ID = "modularmods";
+
+    public static void init() {
+
+    }
+}
---
+++ fabric/src/main/java/net/modularmods/modularguns/fabric/ModularGunsFabric.java
@@ -0,0 +1,11 @@
+package net.modularmods.modularguns.fabric;
+
+import net.modularmods.modularguns.ModularMods;
+import net.fabricmc.api.ModInitializer;
+
+public class ModularGunsFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ModularMods.init();
+    }
+}
